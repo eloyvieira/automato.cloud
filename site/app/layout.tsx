46884709f6816ca5e52d97810c8edd43342d2c81
eloyvieira <eloyvieira@gmail.com>
2026-09-14 18:30:47 -0400

    google analytics

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -97,7 +98,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-HYN6FQHWZG"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HYN6FQHWZG');
+          `}
+        </Script>
+
+      </body>
     </html>
   );
 }
